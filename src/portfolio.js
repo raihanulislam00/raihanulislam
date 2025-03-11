eec8e1710f8e9c3ebd442ff03d0b5eeaafe985bd
Raihanul Islam Nahid <97145938+raihanulislam00@users.noreply.github.com>
2025-03-11 16:38:58 +0600

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -207,9 +207,9 @@ const problemSolving = {
   projects: [
     {
       image: require("./assets/images/cf.png"),
-      projectName: "Cyan/Specialist",
+      projectName: "expert",
       projectDesc:
-        "Highest Rating: 1514,  Top 26.0 percent among 600,000+ users",
+        "Highest Rating: 1672,  Top 26.0 percent among 600,000+ users",
       footerLink: [
         {
           name: "Roll_Num_44",
@@ -221,7 +221,7 @@ const problemSolving = {
       image: require("./assets/images/codechef.png"),
       projectName: "5* / yellow",
       projectDesc:
-        "Highest Rating: 2086,  Top 15.0 percent among 600,000+ users",
+        "Highest Rating: 2174,  Top 15.0 percent among 600,000+ users",
       footerLink: [
         {
           name: "roll_num_44",
@@ -231,9 +231,9 @@ const problemSolving = {
     },
     {
       image: require("./assets/images/at.jpg"),
-      projectName: "Green",
+      projectName: "Cyan",
       projectDesc:
-        "Highest Rating: 1070,  Top 45.0 percent among 600,000+ users",
+        "Highest Rating: 1345,  Top 45.0 percent among 600,000+ users",
       footerLink: [
         {
           name: "Roll_Num_44",
